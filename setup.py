39f0b92918a747a90d6df9eb7cf6e9b1f958ee4e
David Harris <dharris@dharris.io>
2016-06-06 12:38:35 -0600

    Updating pip requirements.
    Update tfwrapper script shebang.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import sys
 from setuptools import setup
 
-requires = ['botocore>=1.4.23',
-            'boto3>=1.3.0',
+requires = ['botocore>=1.3.0',
+            'boto3>=1.2.0',
             's3transfer>=0.0.1']
 
 if sys.version_info[:2] == (2, 6):
@@ -14,7 +14,7 @@ with open('README.rst') as f:
 
 setup(
     name='tfwrapper',
-    version='0.0.1a4',
+    version='0.0.1a7',
     description='''
     Python wrapper for Terraform (https://www.terraform.io) that provides remote state management and basic locking
     ''',

6ec839f5b2a14eb8c79f0323a57f18bf9ee5d5ba
David Harris <dharris@dharris.io>
2016-06-07 11:07:34 -0600

    Change tf environment variable.
    Use shortend `env` variable instead of `environment` in arguments.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst') as f:
 
 setup(
     name='tfwrapper',
-    version='0.0.1a8',
+    version='0.0.1a9',
     description='''
     Python wrapper for Terraform (https://www.terraform.io) that provides remote state management and basic locking
     ''',
